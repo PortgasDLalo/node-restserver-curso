77ef3fcfdca31a86d237dc6ed27ed7d3c77dd28d
= <=>
2019-09-17 13:51:59 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://portgasdlalo:r22lhIO1tJNQI2LS@cluster0-3sy7z.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
